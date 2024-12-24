0f1718f5db7497b2565dbc424da1727df53afaca
swissmint <brianhudsonwhite@outlook.com>
2024-12-24 21:14:20 +0100

    fixed sorting algoritm

--- python cli/sorting.py
+++ python cli/sorting.py
@@ -2,27 +2,37 @@ list_random = [1, 4, 9, 4, 5, 7, 3, 2, 5, 7, 4, 5, 3, 2, 4, 5, 4, 3, 3, 5, 5, 3,
 new_list = [0]
 num  = len(new_list)
 i = 0
-maxmin = 13
-minium = 10000
+index_minimum = 404
+minimum = 404
 counter = 0
-while not counter > len(list_random):
-    print(len(list_random))
+maximum = len(list_random)
+while counter < maximum:
+
+    print(len(list_random)) # 24
     
     for i in range(0, len(list_random)):
-        if list_random[i] < minium:
-            minium = list_random[i]
-            maxmin = i
+        if list_random[i] < minimum:
+            print("old min: ", minimum)
+            minimum = list_random[i]
+            print("new min: ", minimum)
+            index_minimum = i
             
             print("yes", list_random[i])
         else:
             print("nope", list_random[i])
     
-    print("------")
-    list_random.pop(maxmin)
-    print(minium)
-    new_list.append(minium)
-
+    
+    print(list_random)
+    list_random.pop(index_minimum)
+    print(list_random)
+    new_list.append(minimum)
+    print("newww: ", new_list)
     counter += 1
+    print("he: ", counter)
+    minimum = 404
+    print("------")
+
+
 new_list.pop(0) 
 print(new_list)
 
